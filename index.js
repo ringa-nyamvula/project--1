c9f27cedda4e25be8623e488de15a357ea30866a
ringa-nyamvula <99404278+ringa-nyamvula@users.noreply.github.com>
2023-04-11 08:32:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,41 +7,76 @@ function fetchRecipes() {
   fetch(recipesEndpoint)
     .then(response => response.json()) // Parse the response as JSON
     .then(data => {
-     
-    data.forEach(el=> {
-        let parentDiv = document.createElement("div");
-        parentDiv.className = "card";
+      const recipesContainer = document.getElementById('recipes');
+      data.forEach(el => {
+        // Create a card element for the recipe
+        let card = document.createElement('div');
+        card.className = 'card';
 
-        let image = document.createElement("img")
+        // Add the recipe image to the card
+        let image = document.createElement('img');
         image.src = el.strMealThumb;
-        parentDiv.appendChild(image);
+        card.appendChild(image);
 
-        let detailsDiv = document.createElement("div");
-        detailsDiv.className = "card-details"
+        // Add the recipe details to the card
+        let detailsDiv = document.createElement('div');
+        detailsDiv.className = 'card-details';
 
-
-        let name= document.createElement("p")
+        let name = document.createElement('p');
         name.textContent = el.strMeal;
         detailsDiv.appendChild(name);
 
-       
-        let step= document.createElement("p")
-        step.textContent = el.strInstructions
+        let step = document.createElement('p');
+        step.textContent = el.strInstructions;
         detailsDiv.appendChild(step);
 
-        parentDiv.appendChild(detailsDiv);
+        // Add the like button to the card
+        let likeButton = document.createElement('button');
+        likeButton.className = 'like-button';
+        likeButton.textContent = 'Like';
+        likeButton.addEventListener('click', () => {
+          // Increase the like count when the button is clicked
+          likeCount.textContent = Number(likeCount.textContent) + 1;
+        });
 
-        document.querySelector("body").appendChild(parentDiv);
-        
-        
-        
-    
-     
+        // Add the like count to the card
+        let likeCount = document.createElement('span');
+        likeCount.textContent = '0';
+        detailsDiv.appendChild(likeCount);
+
+        // Add the comment section to the card
+        let commentSection = document.createElement('div');
+        commentSection.className = 'comment-section';
+
+        let commentTextarea = document.createElement('textarea');
+        commentTextarea.placeholder = 'Add a comment';
+        commentSection.appendChild(commentTextarea);
+
+        let commentButton = document.createElement('button');
+        commentButton.textContent = 'Add';
+        commentButton.addEventListener('click', () => {
+          let commentText = commentTextarea.value;
+          if (commentText) {
+            let comment = document.createElement('p');
+            comment.className = 'comment';
+            comment.textContent = commentText;
+            commentSection.insertBefore(comment, commentTextarea);
+            commentTextarea.value = '';
+          }
+        });
+        commentSection.appendChild(commentButton);
+
+        detailsDiv.appendChild(likeButton);
+        detailsDiv.appendChild(commentSection);
+
+        card.appendChild(detailsDiv);
+
+        recipesContainer.appendChild(card);
+      });
     })
     .catch(error => {
       console.error('Error fetching recipes:', error);
     });
-})
 }
 
 // Call the fetchRecipes function to display the recipes
